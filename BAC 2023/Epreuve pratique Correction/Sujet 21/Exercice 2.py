6a7f7d1f70bb4b98c408f5f6c34a53679744f67b
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-02-02 16:11:22 +0100

    Sujet 21.2 fix bug

--- BAC 2023/Epreuve pratique Correction/Sujet 21/Exercice 2.py
+++ BAC 2023/Epreuve pratique Correction/Sujet 21/Exercice 2.py
@@ -1,45 +1,45 @@
 
-# class Noeud:
-#     '''
-#     classe implÃ©mentant un noeud d'arbre binaire
-#     '''
+class Noeud:
+    '''
+    classe implÃ©mentant un noeud d'arbre binaire
+    '''
 
-#     def __init__(self, g, v, d):
-#         '''
-#         un objet Noeud possÃ¨de 3 attributs :
-#         - gauche : le sous-arbre gauche,
-#         - valeur : la valeur de l'Ã©tiquette,
-#         - droit : le sous-arbre droit.
-#         '''
-#         self.gauche = g
-#         self.valeur = v
-#         self.droit = d
+    def __init__(self, g, v, d):
+        '''
+        un objet Noeud possÃ¨de 3 attributs :
+        - gauche : le sous-arbre gauche,
+        - valeur : la valeur de l'Ã©tiquette,
+        - droit : le sous-arbre droit.
+        '''
+        self.gauche = g
+        self.valeur = v
+        self.droit = d
 
-#     def __str__(self):
-#         '''
-#         renvoie la reprÃ©sentation du noeud en chaine de caractÃ¨res
-#         '''
-#         return str(self.valeur)
+    def __str__(self):
+        '''
+        renvoie la reprÃ©sentation du noeud en chaine de caractÃ¨res
+        '''
+        return str(self.valeur)
 
-#     def est_une_feuille(self):
-#         '''
-#         renvoie True si et seulement si le noeud est une feuille
-#         '''
-#         return self.gauche is None and self.droit is None
-
-
+    def est_une_feuille(self):
+        '''
+        renvoie True si et seulement si le noeud est une feuille
+        '''
+        return self.gauche is None and self.droit is None
 
 
 
-# def expression_infixe(e) : 
-#     s = str(e)
-#     if e.gauche is not None:
-#         s = "(" + expression_infixe(e.gauche) 
-#     s = s + str(e.valeur)
-#     if e.droit is not None:
-#         s = s + expression_infixe(e.droit) + ")"
-#     return s
 
-# print(expression_infixe(Noeud(Noeud(Noeud(None, 3, None),
-#     '*', Noeud(Noeud(None, 8, None), '+', Noeud(None, 7, None))),
-#     '-', Noeud(Noeud(None, 2, None), '+', Noeud(None, 1, None)))))
+
+def expression_infixe(e) : 
+    s = ""
+    if e.gauche is not None:
+        s = "(" + s + expression_infixe(e.gauche) 
+    s = s + str(e.valeur)
+    if e.droit is not None:
+        s = s + expression_infixe(e.droit) + ")"
+    return s
+
+print(expression_infixe(Noeud(Noeud(Noeud(None, 3, None),
+    '*', Noeud(Noeud(None, 8, None), '+', Noeud(None, 7, None))),
+    '-', Noeud(Noeud(None, 2, None), '+', Noeud(None, 1, None)))))
