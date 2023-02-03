ad03a2dc0c6f70c0caa44e8ba814adc0ff4bf70c
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-02-03 20:54:37 +0100

    Update Exercice 1.py

--- BAC 2023/Epreuve pratique Correction/Sujet 8/Exercice 1.py
+++ BAC 2023/Epreuve pratique Correction/Sujet 8/Exercice 1.py
@@ -1,10 +1,11 @@
 def max_dico(dico):
     """Retourne la clé et la valeur maximale d'un dictionnaire"""
-    max_value = max(dico.values()) # On récupère la valeur maximale du dictionnaire
-    max_key = [k for k, v in dico.items() if v == max_value][0] # On récupère la clé associée à la valeur maximale
-    return (max_key, max_value) # On renvoie la clé et la valeur maximale
-
-
-
+    max_like = 0
+    for pseudo in dico:
+        if dico[pseudo]>max_like:
+            max_pseudo = pseudo
+            max_like=dico[pseudo]
+    return max_pseudo,max_like
+ 
 dico = {'Bob': 102, 'Ada': 201, 'Alice': 103, 'Tim': 50}
 print(max_dico(dico))
