bc2bdae6f58128b83f9d84cf766f903dc7b2c66e
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-02-05 11:22:02 +0100

    update

--- BAC 2023/Epreuve pratique Correction/Sujet 7/Exercice 2.py
+++ BAC 2023/Epreuve pratique Correction/Sujet 7/Exercice 2.py
@@ -1,14 +1,12 @@
-
 romains = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
 
 def traduire_romain(nombre) :
     """ Renvoie l'ecriture decimale du nombre donné en chiffres romains """
-
     if len(nombre) == 1:
-        return romains[nombre]
-
-    elif romains[nombre[0]] >= romains[nombre[1]]:
-        return romains[nombre[0]] + traduire_romain(nombre[1:])
+        return romains[nombre] #
+    elif romains[nombre[0]] >= romains[nombre[1]] :
+        return romains[nombre[0]] + traduire_romain(nombre[1:]) 
     else:
-        return romains[nombre[1]] - romains[nombre[0]] + traduire_romain(nombre[2:])
+        return traduire_romain(nombre[1:]) - romains[nombre[0]]
 
+print(traduire_romain("MCMXCIX"))
