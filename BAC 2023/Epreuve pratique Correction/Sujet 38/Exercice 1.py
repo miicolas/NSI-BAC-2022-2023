27adc05d9a5c3325573c1cfafb1ab8fd83977c22
Nicolas Becharat <78233186+justniicolas@users.noreply.github.com>
2023-02-21 22:29:27 +0000

    38.1 39.1

--- BAC 2023/Epreuve pratique Correction/Sujet 38/Exercice 1.py
+++ BAC 2023/Epreuve pratique Correction/Sujet 38/Exercice 1.py
@@ -1,1 +1,9 @@
+def correspond (mot, mot_a_trous) : 
+    if len(mot) != len(mot_a_trous) :
+        return False
+    for i in range(len(mot)): 
+        if mot [i] != mot_a_trous [i] and mot_a_trous [i] != '*': 
+            return False
+    return True
 
+print(correspond('INFORMATIQUE', 'INFO*MA*IQUE'))
