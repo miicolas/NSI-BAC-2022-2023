8000f44d35ccea1c84b6310f7b8652389763a019
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-02-24 18:36:23 +0100

    24.2 25.2

--- BAC 2023/Epreuve pratique Correction/Sujet 25/Exercice 2.py
+++ BAC 2023/Epreuve pratique Correction/Sujet 25/Exercice 2.py
@@ -1,1 +1,27 @@
+class Arbre:
+    def __init__(self, etiquette):
+        self.v = etiquette
+        self.fg = None
+        self.fd = None
 
+def parcours(arbre, liste):
+    if arbre != None:
+        parcours(arbre.fg, liste)
+        liste.append(arbre.v)
+        parcours(arbre.fd, liste)
+    return liste
+
+def insere(arbre, cle):
+    """ arbre est une instance de la classe Arbre qui implÃ©mente
+        un arbre binaire de recherche.
+    """
+    if  cle < arbre.v :
+        if arbre.fg != None:
+            insere(arbre.fg, cle)
+        else:
+            arbre.fg = Arbre(cle)
+    else:
+        if arbre.fd != None:
+            insere(arbre.fd, cle)
+        else:
+            arbre.fd = Arbre(cle)
