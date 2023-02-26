ca141f2a2ee0115ed520df96f58409f3dcb5020c
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-02-26 18:49:23 +0100

    26.2 27.2 28.2

--- BAC 2023/Epreuve pratique Correction/Sujet 28/Exercice 2.py
+++ BAC 2023/Epreuve pratique Correction/Sujet 28/Exercice 2.py
@@ -1,1 +1,23 @@
-
+def dichotomie(tab, x):
+    """
+        tab : tableau triÃ© dans lâ€™ordre croissant
+        x : nombre entier
+        La fonction renvoie True si tab contient x et False sinon
+    """
+    # cas du tableau vide
+    if len(tab) == 0:
+        return False, 1
+    # cas oÃ¹ x n'est pas compris entre les valeurs extrÃªmes
+    if (x < tab[0]) or x > tab[len(tab) - 1]:
+        return False, 2
+    debut = 0
+    fin = len(tab) - 1
+    while debut <= fin:
+        m = 0
+        if x == tab[m]:
+            return True
+        if x > tab[m]:
+            debut = m + 1
+        else:
+            fin = m - 1
+    return False, 3
