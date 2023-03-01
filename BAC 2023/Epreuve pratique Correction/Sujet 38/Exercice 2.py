976c553459ace792d68058a2bd5d2f34f8787a07
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-03-01 12:55:54 +0100

    36.2 37.2 38.2

--- BAC 2023/Epreuve pratique Correction/Sujet 38/Exercice 2.py
+++ BAC 2023/Epreuve pratique Correction/Sujet 38/Exercice 2.py
@@ -1,1 +1,10 @@
+def est_cyclique(plan):
+    expediteur = 'A'
+    destinataire = plan[expediteur]
+    nb_destinaires = 1
+    
+    while destinataire != expediteur:
+        destinataire = plan[destinataire]
+        nb_destinaires += 1
 
+    return nb_destinaires == len(plan)
