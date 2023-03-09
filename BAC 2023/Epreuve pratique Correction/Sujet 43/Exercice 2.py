f2ed2723b0b7070d3c6b92ca947a7f8c43995334
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-03-09 11:11:12 +0100

    45.2 44.2 43.2 42.2

--- BAC 2023/Epreuve pratique Correction/Sujet 43/Exercice 2.py
+++ BAC 2023/Epreuve pratique Correction/Sujet 43/Exercice 2.py
@@ -1,1 +1,12 @@
-
+def tri_bulles(T):
+    '''
+	Renvoie le tableau T triÃƒÂ© par ordre croissant
+	'''
+    n = len(T)
+    for i in range(n-1,0,-1): # On parcourt le tableau de la fin vers le début
+        for j in range(i):
+            if T[j] > T[i]:
+                temp = T[j]
+                T[j] = T[j+1]
+                T[j+1] = temp 
+    return T
